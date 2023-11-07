443227659ac2a9fc8bfa80fd7f2b6368254140d4
tarunjangid12 <146462019+tarunjangid12@users.noreply.github.com>
2023-11-07 18:15:11 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -79,7 +79,7 @@ async def _batch(event):
 
 async def run_batch(userbot, client, sender, link, _range):
     for i in range(_range):
-        timer = 60
+        timer = 30
         if i < 25: 
             timer = 2
         if i < 50 and i > 25:
