04a0e9042809bd559b634bc6f5afe55bf0ad31dd
tarunjangid12 <146462019+tarunjangid12@users.noreply.github.com>
2024-01-16 09:35:54 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -18,6 +18,8 @@ BOT_TOKEN = "6743200249:AAFegGMnnYHaHZ_zqH8lYk9sMx1CVLm1SD4"
 SESSION = "AQFplNMAe9dQ-mut2Ma2Mtw9S8sUzs-oFu_HVSmyhu3j94lO7Kl3b8SijG9Ywe59TwHObzIuyPEfkh9tB4VOljZmrzXD-8CmELVJPRZt3JUL9OqLYuQyaSLCD-Es1luVhs-Z_pbGo6k-2VAFvvpQ_u3QOfRd8I10x9oFKSk1ETH7kT8x58LfB2R_JEGunOcprJjlsSSxoKuIObOwRK7K7YdUJe6H57jQLI6uWNUk3UMHapXemW88yRQ6UbZoLAm5MamiAwwklw_c5fx5YE2jji8_5ELwSXVaJHI1reFuRzMqEotHhV6lJC2O95dGR-sqm3Lq7A1EmBGZDWFv0UuV-yK42sBaHQAAAAFI7DftAA"
 FORCESUB = "restrictedcontentt"
 AUTH = 5518407661
+auth_str = config("AUTH", default="")
+AUTHORIZED_USERS = [int(user_id.strip()) for user_id in auth_str.split(",") if user_id.strip()]
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
